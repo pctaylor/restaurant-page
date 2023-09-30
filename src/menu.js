af2abdbe0bc16440202e67aceee42e9eb2f83917
Phil Taylor <phil.c.taylor@gmail.com>
2023-09-29 22:23:59 -0400

    menu just needing real images and descriptions

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,26 @@
 export const menuInfo = function(parent) {
-    const menuInfo = document.createElement('div');
-    menuInfo.id = 'menu-info'
-    menuInfo.innerHTML = "fill this later"
-    parent.appendChild(menuInfo);
+
+    //container to hold the grid
+    const menuGrid = document.createElement('div');
+    menuGrid.id = 'menu-grid';
+    parent.appendChild(menuGrid);
+
+    // add the grid cells to the menu's grid container
+    for (let i = 1; i <= 6; i++) {
+        const menuItem = document.createElement('div');
+        menuItem.className = "menu-item";
+        menuGrid.appendChild(menuItem);
+
+        // add the sub-divs for image and description
+        const menuImage = document.createElement('div');
+        menuImage.className = "menu-image";
+        menuImage.innerHTML = i + " image";
+        menuItem.appendChild(menuImage);
+
+        const menuDescription = document.createElement('div');
+        menuDescription.className = "menu-description";
+        menuDescription.innerHTML = i + " description";
+        menuItem.appendChild(menuDescription);
+
+    };
 };
